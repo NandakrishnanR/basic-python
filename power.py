1c5d6d60d7a75ca0365a00e44a21fe3a24082afe
NANDU R <rnandakrishnan2001@gmail.com>
2020-01-20 21:34:51 +0530

    program to find the power of the given number

---
+++ power.py
@@ -0,0 +1,6 @@
+import math
+n=int(input("enter the number "))
+m=int(input("enter the power"))
+print(math.pow(n,m))
+#similar to above program as that of squareroot everything is same but here we need to declare 2 variables
+#this program can also be done using normal code as using math symbol '**'
